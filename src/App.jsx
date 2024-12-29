75f73816b0dcf4265d60afa54c019e5f793490c3
CrashyCrash <ai.crashycrash@gmail.com>
2024-12-29 18:23:46 -0500

    Initial commit from your app

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+// Track page views with Google Analytics
+function usePageViews() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof gtag !== 'undefined') {
+      gtag('event', 'page_view', {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+}
+
+// Wrapper component for page view tracking
+function AppWithPageViews() {
+  usePageViews();
+
+  return (
+    <ErrorBoundary>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </ErrorBoundary>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppWithPageViews />
+    </BrowserRouter>
+  );
+}
+
+export default App;
